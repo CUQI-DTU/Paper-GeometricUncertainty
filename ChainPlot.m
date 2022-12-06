890fc96cda92da83dbc45fd5b640206e18661b98
fhpDTU <73123325+fhpDTU@users.noreply.github.com>
2022-12-06 14:15:19 +0100

    Add files via upload

---
+++ ChainPlot.m
@@ -0,0 +1,88 @@
+%This script produces the chain plots (figure 3,4,5) in the article.
+
+%1000 600 first
+figure('Position',[100 100 1000 600])
+
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot,'defaultTextInterpreter','latex');
+set(groot,'defaultColorbarTickLabelInterpreter','latex');
+%formatspec = '%0.3f';
+
+%Load results
+load('Presentation_LowB3L1.mat')
+%%
+filename = 'my_chain_plot';
+
+%ystart = [0.72, 0.51, 0.30, 0.09];
+%ystart = [0.72,0.50,0.28,0.06] + 0.02;
+ystart = [0.68,0.36,0.04];
+xstart = [0.04 0.295 0.55 0.795]-0.005;
+%xstart = [0.05 0.26 0.57 0.68];
+
+%set(0,'defaultTextInterpreter','latex');
+N_burn = res.setup.Nburn;
+N_trim = 1;
+for i=1:3
+    %ax1 = axes('Position',[xstart(5-i) ystart(1) 0.2 0.2]);
+    %ax2 = axes('Position',[xstart(5-i) ystart(2) 0.2 0.2]);
+    %ax3 = axes('Position',[xstart(5-i) ystart(3) 0.2 0.2]);
+    
+    ax1 = axes('Position',[xstart(1) ystart(i) 0.2 0.25]);
+    ax2 = axes('Position',[xstart(2) ystart(i) 0.2 0.25]);
+    ax3 = axes('Position',[xstart(3) ystart(i) 0.2 0.25]);
+    ax4 = axes('Position',[xstart(4) ystart(i) 0.2 0.25]);
+    
+    if i==1
+        chain = res.lambda_samps;
+        colorstring = '#D95319';
+        colortriplet = [0.8500 0.3250 0.0980];
+        name = '$\lambda$';
+        offset = 1000;
+    elseif i==2
+        chain = res.delta_samps;
+        colorstring = '#0072BD';
+        colortriplet = [0 0.4470 0.7410];
+        name = '$\delta$';
+        offset = 50;
+    elseif i==3
+        chain = res.cor_samps/(17.4*10^(-3));
+        colorstring = '#77AC30';
+        colortriplet = [0.4660 0.6740 0.1880];
+        name = '$c$';
+        offset = 1;
+    end
+    
+    q_lower = quantile(chain(N_burn:end),0.025);
+    q_upper = quantile(chain(N_burn:end),0.975);
+    
+    plot(ax1,1:length(chain),chain,'Color',colorstring)
+    title(ax1,[name ' - chain - full'])
+    xlim(ax1,[1,length(chain)])
+    ylim(ax1,[min(chain)-offset,max(chain)+offset])
+    if i==3
+        ax1.YTick = [0,3,6,9,12];
+    end
+    plot(ax2,(N_burn+1):length(chain),chain(N_burn+1:N_trim:end),'Color',colorstring)
+    yline(ax2,q_lower,'--k')
+    yline(ax2,q_upper,'--k')
+    title(ax2,[name ' - chain'])
+    if i==3
+         %ax2.YAxis.TickLabelFormat = '%.4f';
+         %ax2.XLim = [12.240,12.248];
+    end
+    histogram(ax3,chain(N_burn+1:N_trim:end),'Normalization','probability','Facecolor',colorstring)
+    xline(ax3,q_lower,'--k')
+    xline(ax3,q_upper,'--k')
+    title(ax3,[name ' - histogram'])
+    [~,~,~,acf] = autocorr(chain(N_burn+1:N_trim:end));
+    acf(1).Color = colortriplet;
+    acf(2).Color = [0 0 0];
+    acf(3).Color = [0 0 0];
+    acf(2).LineStyle = '--';
+    acf(3).LineStyle = '--';
+    title(ax4,[name ' - ACF'])
+    xlabel(ax4,[])
+    ylabel(ax4,[])
+end
+saveas(gcf,filename,'epsc')
---
+++ ReconPlot.m
@@ -0,0 +1,99 @@
+%This script produces the plot of the reconstructions for the different
+%methods (figure 6)
+
+figure('Position',[100 100 1000 1000])
+cmin = 0; cmax = 0.13;
+
+x_recon = zeros(2000^2,12);
+cor_vec = zeros(1,12);
+formatspec = '%0.2f';
+
+%Load the results sequentially
+load('Presentation_B3L1.mat');
+x_recon(:,1) = res.X_MAP_ini; x_recon(:,2) = res.x_mean; x_recon(:,3) = res.X_MAP_com; x_recon(:,4) = res.X_MAP_xcorr;
+cor_vec(1) = 0; cor_vec(2) = mean(res.cor_samps(end-1000:end)); cor_vec(3) = res.dx_com*res.setup.sy_true/(res.setup.sy_true + res.setup.dy_true); cor_vec(4) = res.dx_xcorr*res.setup.sy_true/(res.setup.sy_true + res.setup.dy_true);
+
+load('Presentation_LowB3L1.mat')
+x_recon(:,5) = res.X_MAP_ini; x_recon(:,6) = res.x_mean; x_recon(:,7) = res.X_MAP_com; x_recon(:,8) = res.X_MAP_xcorr;
+cor_vec(5) = 0; cor_vec(6) = mean(res.cor_samps(end-1000:end)); cor_vec(7) = res.dx_com*res.setup.sy_true/(res.setup.sy_true + res.setup.dy_true); cor_vec(8) = res.dx_xcorr*res.setup.sy_true/(res.setup.sy_true + res.setup.dy_true);
+
+load('Presentation_B3L1limangle.mat');
+x_recon(:,9) = res.X_MAP_ini; x_recon(:,10) = res.x_mean; x_recon(:,11) = res.X_MAP_com; x_recon(:,12) = res.X_MAP_xcorr;
+cor_vec(9) = 0; cor_vec(10) = mean(res.cor_samps(end-1000:end)); cor_vec(11) = res.dx_com*res.setup.sy_true/(res.setup.sy_true + res.setup.dy_true); cor_vec(12) = res.dx_xcorr*res.setup.sy_true/(res.setup.sy_true + res.setup.dy_true);
+
+%1200 and 400 are the default currently
+region_x = 1100;
+region_y = 500;
+
+%cor_vec = dx_vec;
+%ystart = [0.72, 0.51, 0.30, 0.09];
+%ystart = [0.72,0.50,0.28,0.06] + 0.02;
+ystart = [0.72,0.50,0.28,0.06];
+xstart = [0.05 0.26 0.47 0.68];
+%xstart = [0.05 0.26 0.57 0.68];
+cor_vec = cor_vec/(17.4*10^(-3));
+
+for i=1:3
+    %ax1 = axes('Position',[xstart(5-i) ystart(1) 0.2 0.2]);
+    %ax2 = axes('Position',[xstart(5-i) ystart(2) 0.2 0.2]);
+    %ax3 = axes('Position',[xstart(5-i) ystart(3) 0.2 0.2]);
+    
+    ax1 = axes('Position',[xstart(i) ystart(1) 0.2 0.2]);
+    ax2 = axes('Position',[xstart(i) ystart(2) 0.2 0.2]);
+    ax3 = axes('Position',[xstart(i) ystart(3) 0.2 0.2]);
+    ax4 = axes('Position',[xstart(i) ystart(4) 0.2 0.2]);
+    im = reshape(x_recon(:,i*4),2000,2000);
+    squarezoom_v2(im,4,250,region_x,region_y,ax4,[cmin,cmax]);
+    %imagesc(ax1,im),caxis(ax1,[cmin cmax])
+    title(ax4,['$c$ = ' num2str(cor_vec(i*4),formatspec)],'interpreter','latex')
+    if i==1
+        ylabel(ax4,'XCORR','interpreter','latex')
+    end
+    %xlabel(ax1,'energy bin')
+    %ylabel(ax1,'detector $\times$ measurement')
+
+    im = reshape(x_recon(:,i*4-1),2000,2000);
+    squarezoom_v2(im,4,250,region_x,region_y,ax3,[cmin,cmax])
+    title(ax3,['$c$ = ' num2str(cor_vec(i*4-1),formatspec)],'interpreter','latex')
+    if i==1
+        ylabel(ax3,'COM','interpreter','latex')
+    end
+    %xlabel(ax2,'energy bin')
+    %ylabel(ax2,'detector $\times$ measurement')
+
+    im = reshape(x_recon(:,i*4-2),2000,2000);
+    squarezoom_v2(im,4,250,region_x,region_y,ax2,[cmin,cmax])
+    %title(ax3,'(a) image 3')
+    %xlabel(ax3,'energy bin')
+    %ylabel(ax3,'detector $\times$ measurement')
+    title(ax2,['$c$ = ' num2str(cor_vec(i*4-2),formatspec)],'interpreter','latex')
+    if i==1
+        ylabel(ax2,'MCMC','interpreter','latex')
+    end
+    
+    
+    im = reshape(x_recon(:,i*4-3),2000,2000);
+    squarezoom_v2(im,4,250,region_x,region_y,ax1,[cmin,cmax])
+    if i == 1
+        title(ax1,{'High-dose','$c$ = 0'},'interpreter','latex')
+        ylabel(ax1,'Initial','interpreter','latex')
+    elseif i==2
+        title(ax1, {'Low-dose','$c$ = 0'},'interpreter','latex')
+    elseif i==3
+        title(ax1, {'Short-scan','$c$ = 0'},'interpreter','latex')
+    end
+    
+    set(ax1, 'XTick', []);
+    set(ax1, 'YTick', []);
+    set(ax2, 'XTick', []);
+    set(ax2, 'YTick', []);
+    set(ax3, 'XTick', []);
+    set(ax3, 'YTick', []);
+    set(ax4, 'XTick', []);
+    set(ax4, 'YTick', []);
+end
+colorbar(ax4,'location','southoutside','Position',...
+      [0.05 0.03 0.62 0.02]);
+
+%Save figure
+saveas(gcf,'ReconplotAllMethods_new','epsc')
